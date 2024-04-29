8ca7187c9e89cde129cc75bf0b5df939c1d374fa
Millzy <millzyyt08@gmail.com>
2024-04-29 01:31:08 +0100

    working sanity checks

--- Preloader/src/main.cpp
+++ Preloader/src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Preloader is responsible for doing the final checks before performing the TolCat initialisation steps
  * Tolcat is basically just a library with all the modding functions in it.
- * The preloader checks if the unity version and target game are correct, and then loads TolCat, and then the Mod
+ * The preloader checks if the unity version and target game are correct, and then loads TolCat
 */
 
 #if WIN32
@@ -11,7 +11,41 @@
 // support for linux planned eventually
 #endif
 
-#if WIN32
+#include <filesystem>
+
+#include "command_line.hpp"
+#include "error_handling.hpp"
+#include "sanity_checks.hpp"
+
+bool loadTolCat(bool requireSanity) {
+    char mainModuleName[MAX_PATH];
+    DWORD dataLength = GetModuleFileNameA(nullptr, mainModuleName, sizeof(mainModuleName));
+    if (!dataLength) {
+        HANDLE_SYSTEM_ERROR(GetLastError());
+        return false;
+    }
+
+    std::filesystem::path mainModulePath(mainModuleName);
+    std::filesystem::path dataFolder = mainModulePath.parent_path() / mainModulePath.stem().string().append("_Data");
+
+    bool isClean = true;
+    if (requireSanity) {
+        isClean = unitySanityClean(dataFolder / "globalgamemanagers") && gameSanityClean(dataFolder / "app.info");
+    }
+
+    if (isClean) {
+        std::filesystem::path tolCatPath = mainModulePath.parent_path() / MOD_NAME "_Data" / "TolCat.dll";
+        HMODULE tolCatHandle = LoadLibraryExW(tolCatPath.c_str(), nullptr, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
+
+        if (!tolCatHandle) {
+            HANDLE_SYSTEM_ERROR(GetLastError());
+            return false;
+        }
+    }
+
+    return true;
+}
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "ConstantFunctionResult"
 [[maybe_unused]] BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD fdwReason, [[maybe_unused]] LPVOID lpvReserved) {
@@ -20,16 +54,13 @@
         return TRUE;
     }
 
-    (void)DisableThreadLibraryCalls(hinstDll);
+    bool success = DisableThreadLibraryCalls(hinstDll);
+    if (!success) {
+        HANDLE_SYSTEM_ERROR(GetLastError());
+    }
 
-#else // WIN32
-[[maybe_unused]] __attribute__((constructor)) preloader_init() {
-#endif // WIN32
+    TolCatLaunchArgs launchArgs = getLaunchArgs();
+    return loadTolCat(!hasLaunchArg(launchArgs, TolCatLaunchArgs::FORCE_LOAD));
 
-
-
-#if WIN32
-    return TRUE;
-#endif // WIN32
 }
 #pragma clang diagnostic pop
