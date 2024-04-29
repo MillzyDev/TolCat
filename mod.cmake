a0b8ecd35eb3a8cbec6b5d3e8c8b62799176293a
Millzy <millzyyt08@gmail.com>
2024-04-29 01:31:35 +0100

    properties for testing

--- mod.cmake
+++ mod.cmake
@@ -1,3 +1,8 @@
 set(MOD_NAME "Placeholder Name")
 set(MOD_VERSION "0.1.0")
 set(MOD_ENTRY_POINT "mod_init")
+
+set(GAME_COMPANY "Stress Level Zero")
+set(GAME_NAME "BONELAB")
+
+set(GAME_AGNOSTIC OFF) # this should always be off
--- unity.cmake
+++ unity.cmake
@@ -2,3 +2,8 @@ set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.5f1")
 
 set(UNITY_MAJOR "2021")
 set(UNITY_MINOR "3")
+set(UNITY_PATCH "5f1")
+
+set(UNITY_MAJOR_AGNOSTIC OFF) # not recommended
+set(UNITY_MINOR_AGNOSTIC OFF) # not as bad
+set(UNITY_PATCH_AGNOSTIC ON) # probably fine
