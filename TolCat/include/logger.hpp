3e93cc0adf5f7c163575fc406399db938596035f
Millzy <millzyyt08@gmail.com>
2024-05-01 18:44:38 +0100

    method for adding log outputs

--- TolCat/include/logger.hpp
+++ TolCat/include/logger.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <string>
+#include <type_traits>
 
 #include "tolcat_config.hpp"
 
 namespace TolCat {
     class TOLCAT_API LoggerOutputInterface {
     public:
-        virtual void logInfo(std::string timestamp, std::string nameSection, std::string messageSection);
-        virtual void logWarn(std::string timestamp, std::string nameSection, std::string messageSection);
-        virtual void logError(std::string timestamp, std::string nameSection, std::string messageSection);
+        virtual void logInfo(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection);
+        virtual void logWarn(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection);
+        virtual void logError(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection);
     };
 
     class TOLCAT_API LoggerFileOutput : public LoggerOutputInterface {
@@ -19,9 +21,9 @@ namespace TolCat {
         std::ofstream logFileStream;
     public:
         explicit LoggerFileOutput(const std::filesystem::path &logsDir);
-        void logInfo(std::string timestamp, std::string nameSection, std::string messageSection) override;
-        void logWarn(std::string timestamp, std::string nameSection, std::string messageSection) override;
-        void logError(std::string timestamp, std::string nameSection, std::string messageSection) override;
+        void logInfo(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
+        void logWarn(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
+        void logError(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
     };
 
     class TOLCAT_API LoggerConsoleOutput : public LoggerOutputInterface {
@@ -29,12 +31,20 @@ namespace TolCat {
         std::ofstream conOutStream;
     public:
         LoggerConsoleOutput();
-        void logInfo(std::string timestamp, std::string nameSection, std::string messageSection) override;
-        void logWarn(std::string timestamp, std::string nameSection, std::string messageSection) override;
-        void logError(std::string timestamp, std::string nameSection, std::string messageSection) override;
+        void logInfo(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
+        void logWarn(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
+        void logError(const std::string &timestamp, const std::string &nameSection, const std::string &messageSection) override;
     };
 
     class TOLCAT_API Logger {
+    private:
+        std::vector<LoggerOutputInterface> loggerOutputs;
 
+    public:
+        template<class TLoggerOutput>
+        inline void addLoggerOutput(TLoggerOutput loggerOutput) {
+            static_assert(std::is_base_of_v<LoggerOutputInterface, TLoggerOutput>, "Class of TLoggerOutput must derive from LoggerOutputInterface");
+            this->loggerOutputs.push_back(loggerOutput);
+        }
     };
 }
