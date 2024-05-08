8d617eb4dab5ffdc1d03f74f046349e424e8d976
Millzy <millzyyt08@gmail.com>
2024-05-08 16:32:54 +0100

    copy script (windows)

---
+++ copy-to-target.ps1
@@ -0,0 +1,22 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [String] $modName = "",
+
+    [Parameter(Mandatory = $true)]
+    [String] $targetGameDir = "",
+
+    [Parameter(Mandatory = $true)]
+    [String] $buildDir = ""
+)
+
+$proxySource = Join-Path -Path $buildDir -ChildPath "version.dll"
+$proxyDest = $targetGameDir
+Copy-Item $proxySource -Destination $proxyDest
+
+$preloaderSource = Join-Path -Path $buildDir -ChildPath "Preloader.dll"
+$proxyDest = Join-Path -Path $targetGameDir -ChildPath $modName"_Data"
+Copy-Item $preloaderSource -Destination $proxyDest
+
+$tolCatSource = Join-Path -Path $buildDir -ChildPath "TolCat.dll"
+$tolCatDest = Join-Path -Path $targetGameDir -ChildPath $modName"_Data"
+Copy-Item $tolCatSource -Destination $tolCatDest
