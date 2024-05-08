fb821b551273241ff524c75684951460a1ec6ddc
Millzy <millzyyt08@gmail.com>
2024-05-08 16:32:21 +0100

    entrypoint hopefully

--- TolCat/src/main.cpp
+++ TolCat/src/main.cpp
@@ -1,5 +1,17 @@
 #include "launch_flags.hpp"
+#include "logger.hpp"
+#include "files.hpp"
 
-void tolCatLaunch(TolCatLaunchArgs launchArgs) {
-
+extern "C" __declspec(dllexport) void launchTolCat(TolCatLaunchArgs launchArgs) {
+    TolCat::Logger::addLoggerOutput(
+            TolCat::LoggerFileOutput(
+                    TolCat::Files::getLogsDir()
+                    )
+            );
+    
+    if (hasLaunchArg(launchArgs, TolCatLaunchArgs::DEBUG_CONSOLE)) {
+        TolCat::Logger::addLoggerOutput(
+                TolCat::LoggerConsoleOutput()
+                );
+    }
 }
