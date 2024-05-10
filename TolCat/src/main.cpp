f882225b40276411594dc990060d055ed990c8d9
Millzy <millzyyt08@gmail.com>
2024-05-10 18:49:24 +0100

    test logger

--- TolCat/src/main.cpp
+++ TolCat/src/main.cpp
@@ -1,21 +1,23 @@
+#include <memory>
+
 #include "launch_flags.hpp"
 #include "logger.hpp"
 #include "files.hpp"
+
+TolCat::Logger tolCatLogger_("TolCat");
 
 extern "C" [[maybe_unused]] __declspec(dllexport) void launchTolCat(TolCatLaunchArgs launchArgs) {
     TolCat::Files::createLogsDir();
 
     // Add the log file output
     TolCat::Logger::addLoggerOutput(
-            TolCat::LoggerFileOutput(
-                    TolCat::Files::getLogsDir()
-                    )
+            std::make_unique<TolCat::LoggerFileOutput>(TolCat::Files::getLogsDir())
             );
 
     // Add the debug console output
     if (hasLaunchArg(launchArgs, TolCatLaunchArgs::DEBUG_CONSOLE)) {
-        TolCat::Logger::addLoggerOutput(
-                TolCat::LoggerConsoleOutput()
-                );
+        TolCat::Logger::addLoggerOutput(std::make_unique<TolCat::LoggerConsoleOutput>());
     }
+
+    tolCatLogger_.info("Logger initialised.");
 }
