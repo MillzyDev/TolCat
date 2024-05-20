5bcdc392d2d6f63fbb48ec24bae8b4adfbe12bbc
Millzy <millzyyt08@gmail.com>
2024-05-20 18:06:12 +0100

    gluon logger interface

---
+++ TolCat/include/gluon_logger.hpp
@@ -0,0 +1,19 @@
+#ifndef TOLCAT_GLUON_LOGGER_HPP_
+#define TOLCAT_GLUON_LOGGER_HPP_
+
+#include "logger.hpp"
+#include "Gluon/include/gluon_logging.hpp"
+
+namespace TolCat {
+    class GluonLogger : public Gluon::Logging::LoggerAdapter {
+    private:
+        static TolCat::Logger gluonLogger;
+    public:
+        void logNeutral(const std::string &message) override;
+        void logInfo(const std::string &message) override;
+        void logWarn(const std::string &message) override;
+        void logError(const std::string &message) override;
+    };
+}
+
+#endif
---
+++ TolCat/src/gluon_logger.cpp
@@ -0,0 +1,23 @@
+#include "gluon_logger.hpp"
+
+#include "logger.hpp"
+
+namespace TolCat {
+    TolCat::Logger GluonLogger::gluonLogger("Gluon");
+
+    void GluonLogger::logNeutral(const std::string &message) {
+        TolCat::Logger::neutral("{}", message);
+    }
+
+    void GluonLogger::logInfo(const std::string &message) {
+        gluonLogger.info("{}", message);
+    }
+
+    void GluonLogger::logWarn(const std::string &message) {
+        gluonLogger.warn("{}", message);
+    }
+
+    void GluonLogger::logError(const std::string &message) {
+        gluonLogger.warn("{}", message);
+    }
+}
