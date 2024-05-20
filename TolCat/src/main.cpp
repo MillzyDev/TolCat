4f8eef8182ea5ea84f1f35f234117f096a709dd6
Millzy <millzyyt08@gmail.com>
2024-05-20 18:06:23 +0100

    initialise gluon

--- TolCat/src/main.cpp
+++ TolCat/src/main.cpp
@@ -3,6 +3,11 @@
 #include "launch_flags.hpp"
 #include "logger.hpp"
 #include "files.hpp"
+#include "gluon_logger.hpp"
+
+#include "Gluon/include/gluon_logging.hpp"
+#include "Gluon/include/xref_helpers.hpp"
+#include "Gluon/include/il2cpp_functions.hpp"
 
 TolCat::Logger tolCatLogger_("TolCat");
 
@@ -20,4 +25,12 @@ extern "C" [[maybe_unused]] __declspec(dllexport) void launchTolCat(TolCatLaunch
     }
 
     tolCatLogger_.info("Logger initialised.");
+
+    tolCatLogger_.info("Initialising Gluon...");
+    Gluon::Logging::Logger::setLoggerAdapter(
+            std::make_unique<TolCat::GluonLogger>()
+            );
+    Gluon::XrefHelpers::initialiseCapstone();
+    Gluon::Il2CppFunctions::Init();
+    tolCatLogger_.info("Finished Gluon initialisation!");
 }
