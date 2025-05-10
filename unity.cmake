de29a3bd7f2465c8a6feb15b644c76d40bd17c62
Millz <millzyyt08@gmail.com>
2025-05-10 10:21:14 +0100

    new unity ver

--- unity.cmake
+++ unity.cmake
@@ -1,8 +1,8 @@
-set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.5f1")
+set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.16f1")
 
 set(UNITY_MAJOR "2021")
 set(UNITY_MINOR "3")
-set(UNITY_PATCH "5f1")
+set(UNITY_PATCH "16f1")
 
 set(UNITY_MAJOR_AGNOSTIC OFF) # not recommended
 set(UNITY_MINOR_AGNOSTIC OFF) # not as bad
