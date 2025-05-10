9ee3edb321f3099443ef1f394c38d0509af1abf8
Millz <millzyyt08@gmail.com>
2025-05-10 10:58:23 +0100

    fix copying to target

--- copy-to-target.ps1
+++ copy-to-target.ps1
@@ -13,14 +13,18 @@ $proxySource = Join-Path -Path $buildDir -ChildPath "version.dll"
 $proxyDest = $targetGameDir
 Copy-Item $proxySource -Destination $proxyDest -Force
 
+$modDataName = -join($modName, "_Data")
+$modDataDir = Join-Path -Path $targetGameDir -ChildPath $modDataName
+New-Item -Type Directory -Path $targetGameDir -Name $modName"_Data" -Force
+
 $preloaderSource = Join-Path -Path $buildDir -ChildPath "Preloader.dll"
-$proxyDest = Join-Path -Path $targetGameDir -ChildPath $modName"_Data"
+$proxyDest = Join-Path -Path $modDataDir -ChildPath "Preloader.dll"
 Copy-Item $preloaderSource -Destination $proxyDest -Force
 
 $tolCatSource = Join-Path -Path $buildDir -ChildPath "TolCat.dll"
-$tolCatDest = Join-Path -Path $targetGameDir -ChildPath $modName"_Data"
+$tolCatDest = Join-Path -Path $modDataDir -ChildPath "TolCat.dll"
 Copy-Item $tolCatSource -Destination $tolCatDest -Force
 
 $gluonSource = Join-Path -Path $buildDir -ChildPath "Gluon/Gluon.dll"
-$gluonDest = Join-Path -Path $targetGameDir -ChildPath $modName"_Data"
+$gluonDest = Join-Path -Path $modDataDir -ChildPath "Gluon.dll"
 Copy-Item $gluonSource -Destination $gluonDest -Force
