b08a3114c06870f2da8b7adcc3e8a5b1618fdd90
Millz <millzyyt08@gmail.com>
2025-05-15 19:54:28 +0100

    aaaaaaaaaaaaaa

--- TolCat/include/tolcat_config.hpp
+++ TolCat/include/tolcat_config.hpp
@@ -2,9 +2,11 @@
 #define TOLCAT_TOLCAT_CONFIG_H_
 
 #ifdef TOLCAT_EXPORT
-#define TOLCAT_API __declspec(dllexport)
+//#define TOLCAT_API __declspec(dllexport)
+#define TOLCAT_API __attribute__((visibility("default")))
 #else // TOLCAT_EXPORT
-#define TOLCAT_API __declspec(dllimport)
+//#define TOLCAT_API __declspec(dllimport)
+#define TOLCAT_API
 #endif // TOLCAT_EXPORT
 
 #endif // TOLCAT_TOLCAT_CONFIG_H_
