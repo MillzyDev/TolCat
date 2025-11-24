1d1b294d7bb8038a4cdbcea17f940b575aed0dda
millz <f.w.b.mills@gmail.com>
2025-11-24 09:38:55 +0000

    rework unity path locator

--- unity.cmake
+++ unity.cmake
@@ -1,8 +1,9 @@
-set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.49f1")
-
 set(UNITY_MAJOR "6000")
 set(UNITY_MINOR "0")
-set(UNITY_PATCH "49f1")
+set(UNITY_PATCH "62")
+set(UNITY_DECORATOR "f1")
+
+set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_MAJOR}.${UNITY_MINOR}.${UNITY_PATCH}${UNITY_DECORATOR}")
 
 set(UNITY_MAJOR_AGNOSTIC OFF) # not recommended
 set(UNITY_MINOR_AGNOSTIC OFF) # not as bad
