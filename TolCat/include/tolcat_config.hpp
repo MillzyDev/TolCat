dd0180b0264310c1500e4a5bfcb479f59d695ab8
millz <f.w.b.mills@gmail.com>
2025-11-29 12:52:45 +0000

    properly support exports on multiple platforms

--- TolCat/include/tolcat_config.hpp
+++ TolCat/include/tolcat_config.hpp
@@ -2,11 +2,21 @@
 #define TOLCAT_TOLCAT_CONFIG_H_
 
 #ifdef TOLCAT_EXPORT
-//#define TOLCAT_API __declspec(dllexport)
+
+#ifdef _WIN32
+#define TOLCAT_API __declspec(dllexport)
+#else // _WIN32
 #define TOLCAT_API __attribute__((visibility("default")))
+#endif // _WIN32
+
 #else // TOLCAT_EXPORT
-//#define TOLCAT_API __declspec(dllimport)
+
+#ifdef _WIN32
+#define TOLCAT_API __declspec(dllimport)
+#else // _WIN32
 #define TOLCAT_API
+#endif // _WIN32
+
 #endif // TOLCAT_EXPORT
 
 #endif // TOLCAT_TOLCAT_CONFIG_H_
